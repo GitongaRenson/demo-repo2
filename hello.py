7fdd2dff51a3c9258cec763969a4f6fa5a037a77
Renson Gitonga <blackhatcoded@gmail.com>
2023-10-03 23:15:46 +0300

    added the basic programs created during my python basics course

---
+++ hello.py
@@ -0,0 +1,30 @@
+# A welcome message to start of python
+# learning this from freecode camp youtube channel
+
+# Assigning variables
+#Don't assign variables starting with numbers
+#Some words are reserved and can't be used as variable
+#Some character like - and ! cannot be used together with the variables
+
+
+line01 = '********************'
+line02 = '*                  *'
+line03 = '*     WELCOME!     *'
+meaning = 42
+
+# running the code
+ 
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+print(line01)
+
+
+print(' ')
+if meaning > 10:
+      print(' What the heck')
+else:
+      print(' Awesome')
