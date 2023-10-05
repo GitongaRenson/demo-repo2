2fbfa239480b019b8e9c68fb4a9b74fd750ced70
Renson Gitonga <blackhatcoded@gmail.com>
2023-10-05 07:54:06 +0300

    user input and lists and tuples input

--- rps.py
+++ rps.py
@@ -1,30 +1,41 @@
 import sys
 import random
-print('')
+from enum import Enum
 
-playerchoice = input('Enter... \n1 for Rock,\n2 for Paper, \n3 for Scissors: \n\n')
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+
+print("")
+
+playerchoice = input("Enter... \n1 for Rock,\n2 for Paper, \n3 for Scissors: \n\n")
+
 player = int(playerchoice)
 
 if player < 1 or player > 3:
-  sys.exit('You must enter either: \n 1, 2, or 3.')
+    sys.exit("You must enter either: \n 1, 2, or 3.")
 
-computerchoice = random.choice('123')
+computerchoice = random.choice("123")
 
 computer = int(computerchoice)
 
-print('You chose ', player)
-print('Computer chose ', computerchoice)
+print("You chose " + str(RPS(player)).replace('RPS.', '') + ".")
+print("Computer chose " + str(RPS(computer)).replace('RPS.', '') + ".")
 
 
-#determining who the winner is using this control flow statement 
+# determining who the winner is using this control flow statement
 
 if computer == 2 and player == 1:
-  print('You lost')
+    print("😣 You lost")
 elif computer == 1 and player == 3:
-  print('You lost')
+    print("😣 You lost")
 elif computer == 3 and player == 2:
-  print('You lost')
+    print("😣 You lost")
 elif computer == player:
-  print('The game was a draw. \n Play again')
+    print("The game was a draw. \n Play again")
 else:
-  print('You won')
+    print("🎉 You Won")
+3
